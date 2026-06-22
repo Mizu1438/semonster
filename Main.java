2a4a3774447763fcd2b4443bed6cd7c393750418
m02530163 <m1m02530163@oit.ac.jp>
2026-06-22 14:14:14 +0900

    メッセージ変更

--- Main.java
+++ Main.java
@@ -1,24 +1,17 @@
-
 import java.util.Random;
 
-public class Monster {
-  String name;
-  int rare;// 1:normal,2:uncommon,3:rare,4:ultra rare
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("SEMONSTER GAME");
+    System.out.println("Show a new monster!");
 
-  Monster() {
-    Random random = new Random();
-    this.name = this.summonMonster(random.nextInt(5));
-    this.rare = random.nextInt(5);
-  }
-
-  String summonMonster(int mnumber) {
-    String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
-    return monsters[mnumber];
-  }
-
-  @Override
-  public String toString() {
-
-    return this.name + ":レア度[" + this.rare + "]";
+    // Playerクラスのオブジェクトを生成（new）する
+    Player player = new Player();
+    
+    // drawMonsters()を呼び出して、モンスターを5体ドローする
+    player.drawMonsters();
+    
+    // showDeck()を呼び出して、ドローしたモンスターのデッキ内容を表示する
+    player.showDeck();
   }
 }
