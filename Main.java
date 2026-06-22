d67f176834bd02665a3a361d99ed652c2052fae3
m02530163 <m1m02530163@oit.ac.jp>
2026-06-22 15:50:19 +0900

    Playerクラスとdrawmonster実装

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        // 最初のメッセージを出力
         System.out.println("SEMONSTER GAME");
         System.out.println("Show a new monster!");
         
-        // Monsterクラスのオブジェクトを生成（召喚）
-        Monster monster = new Monster();
-        
-        // モンスターの情報を出力
-        System.out.println(monster.toString());
+        // Playerオブジェクトを追加し、メソッドを呼び出す
+        Player player = new Player();
+        player.drawMonsters();
+        player.showDeck();
     }
 }
