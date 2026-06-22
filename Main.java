2fa2c3fbe9f7743198e250b26aefb04ba080dddd
m02530163 <m1m02530163@oit.ac.jp>
2026-06-22 15:34:51 +0900

    タイトルメッセージ変更

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         // 最初のメッセージを出力
-        System.out.println("SEMonster!");
+        System.out.println("SEMONSTER GAME");
+        System.out.println("Show a new monster!");
         
         // Monsterクラスのオブジェクトを生成（召喚）
         Monster monster = new Monster();
