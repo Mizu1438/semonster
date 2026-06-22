92b455a9fd74b62daa52f9378972cc17f6b2e435
m02530163 <m1m02530163@oit.ac.jp>
2026-06-22 15:19:01 +0900

    summon monster処理を実装

--- Main.java
+++ Main.java
@@ -1,17 +1,12 @@
-import java.util.Random;
-
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("SEMONSTER GAME");
-    System.out.println("Show a new monster!");
-
-    // Playerクラスのオブジェクトを生成（new）する
-    Player player = new Player();
-    
-    // drawMonsters()を呼び出して、モンスターを5体ドローする
-    player.drawMonsters();
-    
-    // showDeck()を呼び出して、ドローしたモンスターのデッキ内容を表示する
-    player.showDeck();
-  }
+    public static void main(String[] args) {
+        // 最初のメッセージを出力
+        System.out.println("SEMonster!");
+        
+        // Monsterクラスのオブジェクトを生成（召喚）
+        Monster monster = new Monster();
+        
+        // モンスターの情報を出力
+        System.out.println(monster.toString());
+    }
 }
