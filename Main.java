43c49e2f7a62fbcfd596de0a89f8f25747b5bdf2
m02530163 <m1m02530163@oit.ac.jp>
2026-06-22 14:41:54 +0900

    add semonster main class

--- Main.java
+++ Main.java
@@ -2,16 +2,5 @@ import java.util.Random;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMONSTER GAME");
-    System.out.println("Show a new monster!");
-
-    // Playerクラスのオブジェクトを生成（new）する
-    Player player = new Player();
-    
-    // drawMonsters()を呼び出して、モンスターを5体ドローする
-    player.drawMonsters();
-    
-    // showDeck()を呼び出して、ドローしたモンスターのデッキ内容を表示する
-    player.showDeck();
-  }
+    System.out.println("SEmonster!");
 }
